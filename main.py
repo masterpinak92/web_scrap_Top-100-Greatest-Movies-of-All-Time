809e286d9ea0524756f0af5c5e7df2ac6eadb78f
pmehta <111711817+masterpinak92@users.noreply.github.com>
2023-01-24 19:57:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# Top 100 Greatest Movies of All Time (The Ultimate List) - Web Scraping project using Beautiful Soup
+# author : Pinak Mehta
+
 import requests
 from bs4 import BeautifulSoup
 
+
+# make a get request if https://www.imdb.com/list/ls055592025/
 response = requests.get(
     'https://www.imdb.com/list/ls055592025/')
 
@@ -11,12 +16,17 @@ movie_number = soup.find_all(
 
 movie_name = soup.find_all(name='h3', class_="lister-item-header")
 
+# create a list of movies numbers
 movie_number_list = [(movie_number[i].getText())
                      for i in range(len(movie_number))]
+
+# create a list of movies names
 movie_name_list = [(movie_name[i].a.getText()) for i in range(len(movie_name))]
 
 # print(movie_number_list)
 # print(movie_name_list)
+
+# function to print .txt file
 
 
 def list_100_movies():
@@ -26,4 +36,5 @@ def list_100_movies():
             f.write(f'{movie_number_list[i]}) {movie_name_list[i]}\n')
 
 
+# call the funcrion
 list_100_movies()
